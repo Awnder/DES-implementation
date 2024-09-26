92c5175e5e8870b19786e108b11c47b7c40740f1
Andrew Shiroma <amshiroma@zoho.com>
2024-09-26 10:05:58 -0700

    added constant tables from tallman

--- cui_des.py
+++ cui_des.py
@@ -1,3 +1,8 @@
+import des_constants_permutation_tables as permutation_tables
+import des_constants_sbox_tables as sbox_tables
+import des_constants_subkey_tables as subkey_tables
+import des_tests_subkey as subkey_test
+
 def _add_padding(message):
     ''' Returns the message with extra padding (ie: 2 bytes w padding -> \x02\x02).
         used tallman's code
@@ -53,49 +58,23 @@ def _hex_print(block, length=16):
 
 def _generate_subkeys(key):
     ''' Generates 16 subkeys from a 64 bit key. Used gpt-4o to write '''
-    permute_56bit = [
-      56, 48, 40, 32, 24, 16, 8,
-      1, 55, 47, 39, 31, 23, 15,
-      7, 2, 54, 46, 38, 30, 22,
-      14, 6, 3, 53, 45, 37, 29,
-      52, 44, 36, 28, 20, 12, 4,
-      5, 51, 43, 35, 27, 19, 11,
-      3, 6, 50, 42, 34, 26, 18,
-      10, 13, 5, 25, 17, 9, 4
-    ]
-
-    permute_48bit = [
-      14, 17, 11, 24,  1,  5,  3, 28,
-      15,  6, 21, 10, 23, 19, 12,  4,
-      26,  8, 16,  7, 27, 20, 13,  2,
-      41,  32, 31, 37, 47, 30, 40, 38,
-      33, 45, 34, 48, 36, 29, 39, 42,
-      35, 44, 46, 43, 39,  36, 29, 32
-    ]
-
-    # Number of left shifts per round
-    SHIFT_SCHEDULE = [
-      1, 1, 2, 2, 2, 2, 2, 2,
-      1, 2, 2, 2, 2, 2, 2, 1
-    ]
-
     key = _bytes_to_bit_array(key)
     
     # permutes 64 bit key into 56bits
-    permuted_key = _permute(key, permute_56bit)
+    permuted_key = _permute(key, subkey_tables._KEY_PERMUTATION1)
     
     # Split into C and D
     C = permuted_key[:28]
     D = permuted_key[28:]
     
     subkeys = []
-    for shift in SHIFT_SCHEDULE: # shifts to randomize new key
+    for shift in subkey_tables._KEY_SHIFT: # shifts to randomize new key
         # Left shift C and D
         C = C[shift:] + C[:shift]
         D = D[shift:] + D[:shift]
         
         # permutes 56 key into 48 bit subkeys
-        subkey = _permute(C+D, permute_48bit)
+        subkey = _permute(C+D, subkey_tables._KEY_PERMUTATION2)
         subkeys.append(subkey)
     
     return subkeys
@@ -124,39 +103,18 @@ def _feistel_round(L, R, subkey):
 
 def _encrypt_block(block, subkeys):
     ''' Used gpt-4o to generate permutation tables and give advice on how to structure loop '''
-    _INIT_PERMUTATION = [
-      58, 50, 42, 34, 26, 18, 10, 2,
-      60, 52, 44, 36, 28, 20, 12, 4,
-      62, 54, 46, 38, 30, 22, 14, 6,
-      64, 56, 48, 40, 32, 24, 16, 8,
-      57, 49, 41, 33, 25, 17, 9, 1,
-      59, 51, 43, 35, 27, 19, 11, 3,
-      61, 53, 45, 37, 29, 21, 13, 5,
-      63, 55, 47, 39, 31, 23, 15, 7
-    ]
-    _FINAL_PERMUTATION = [
-      40, 8, 48, 16, 56, 24, 64, 32,
-      39, 7, 47, 15, 55, 23, 63, 31,
-      38, 6, 46, 14, 54, 22, 62, 30,
-      37, 5, 45, 13, 53, 21, 61, 29,
-      36, 4, 44, 12, 52, 20, 60, 28,
-      35, 3, 43, 11, 51, 19, 59, 27,
-      34, 2, 42, 10, 50, 18, 58, 26,
-      33, 1, 41, 9, 49, 17, 57, 25
-    ]
-
     # split 64 bit block into 32 bit halves
     L = block[:len(block)//2]
     R = block[len(block)//2:]
 
-    block = _permute(block, _INIT_PERMUTATION)
+    block = _permute(block, permutation_tables._INIT_PERMUTATION)
     
     for i in range(16):
       L, R = _feistel_round(L, R, subkeys[i])
 
     block = R + L # combine halves for ciphertext
        
-    block = _permute(block, _FINAL_PERMUTATION)
+    block = _permute(block, permutation_tables._FINAL_PERMUTATION)
 
     return block
 
